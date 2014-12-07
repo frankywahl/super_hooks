c715b110b2c346598c958d5caf2baf104f90a847
Franky W <frankywahl@users.noreply.github.com>
2014-12-06 18:54:48 -0800

    Started listing hooks

--- lib/super_hooks/hooks.rb
+++ lib/super_hooks/hooks.rb
@@ -19,5 +19,36 @@ module SuperHooks
         pre-push
       )
     end
+
+    #
+    # Returns an array of hooks installed: their path
+    #
+    #
+    def list
+      list = user_hooks + global_hooks + project_hooks
+      list.select{|f| File.file? f}
+    end
+
+    def list_with_descriptions
+      list.reduce({}) do |hash, file|
+        hash[file] = `#{file} --about`
+        hash
+      end
+    end
+
+    private
+
+    def global_hooks
+      Dir[""]
+    end
+
+    def user_hooks
+      Dir["#{ENV['HOME']}/.git_hooks/**/*"]
+    end
+
+    def project_hooks
+      Dir["#{Git.repository}/.git/git_hooks/**/*"]
+    end
+
   end
 end
--- spec/hooks_spec.rb
+++ spec/hooks_spec.rb
@@ -24,4 +24,54 @@ describe SuperHooks::Hooks do
       expect(self.described_class.list).to eql list
     end
   end
+
+  describe "installed hooks" do
+
+    let(:hooks) { self.described_class.new }
+
+    let(:file) {
+      file = "#{@repository.path}/.git/git_hooks/commit-msg/foo"
+      dirname = File.dirname(file)
+
+      FileUtils.mkdir_p(dirname) unless File.directory? dirname
+
+      content = <<-EOF.gsub(/^\s+/, '')
+        #!/bin/bash
+        function foo() {
+          return
+        }
+        case "${1}" in
+          --about )
+            echo -n "Just a check to see if it all works"
+            ;;
+
+          * )
+            foo "#@"
+            ;;
+        esac
+
+      EOF
+
+      File.open(file, 'w', 0755) do |f|
+        f.puts content
+      end
+      file
+    }
+
+
+    describe "#list_with_description" do
+      it "includes project hooks with their description" do
+        file # just create it
+        expect(hooks.list_with_descriptions).to include("#{file}" => "Just a check to see if it all works")
+      end
+    end
+
+    describe "#list" do
+      it "returns an array of active hooks" do
+        file
+        expect(hooks.list).to match_array(file)
+      end
+    end
+
+  end
 end

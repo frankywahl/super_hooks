53ec8a237b257a99a2dba49693c7cc827552c03a
Franky W <frankywahl@users.noreply.github.com>
2014-12-14 15:34:35 -0800

    Allow for a pre-release version

--- lib/super_hooks/version.rb
+++ lib/super_hooks/version.rb
@@ -8,11 +8,13 @@ module SuperHooks
     MINOR = 0
     # Patch level
     PATCH = 1
+    # Preview
+    PRE = nil
 
     # Return the current version of SuperHooks
     # following the semantics versioning
     def self.to_s
-      [MAJOR, MINOR, PATCH].join(".")
+      [MAJOR, MINOR, PATCH, PRE].compact.join(".")
     end
   end
 

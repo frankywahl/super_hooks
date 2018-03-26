8f758a7a1e5b46f20fab67b652fd85170a0ab71f
Franky W <frankywahl@users.noreply.github.com>
2018-03-26 11:56:22 -0700

    Make hooks run in parallel

--- cmd/run.go
+++ cmd/run.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"os"
-	"strings"
+	"os/exec"
+	"sync"
 
 	"github.com/frankywahl/super_hooks/hook"
 	"github.com/spf13/cobra"
@@ -20,33 +23,77 @@ var runCmd = &cobra.Command{
 	Args:    cobra.MinimumNArgs(1),
 	Example: "super_hooks run pre-commit",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		errors := []error{}
-
-		for _, cmd := range hook.For(args[0]) {
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			cmd.Args = append(cmd.Args, args[1:]...)
-			if Verbose {
-				if len(args[1:]) > 0 {
-					fmt.Printf("Running %s hook %s with no arguments\n", args[0], cmd.Path)
-				} else {
-					fmt.Printf("Running %s hook %s with arguments %s\n", args[0], cmd.Path, strings.Join(args[1:], ","))
-				}
-			}
-			if err := cmd.Run(); err != nil {
-				errors = append(errors, err)
-			}
-		}
-
-		if len(errors) > 0 {
+		if err := runCommands(hook.For(args[0]), args[1:]); err != nil {
+			fmt.Fprintf(os.Stderr, "error running commands %v", err)
 			os.Exit(1)
-		} else {
-			os.Exit(0)
 		}
+		os.Exit(0)
 		return nil
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(runCmd)
+}
+
+func runCommands(cmds []*exec.Cmd, args []string) error {
+	stoutc := make(chan string)
+	sterrc := make(chan string)
+	errc := make(chan *exec.Cmd)
+
+	var wg sync.WaitGroup
+	wg.Add(len(cmds))
+
+	go func() {
+		wg.Wait()
+		close(stoutc)
+		close(sterrc)
+		close(errc)
+	}()
+
+	for _, cmd := range cmds {
+		go func(c *exec.Cmd) {
+			defer wg.Done()
+			stdout := bytes.NewBuffer(nil)
+			stderr := bytes.NewBuffer(nil)
+			c.Stdout = stdout
+			c.Stderr = stderr
+			c.Args = args
+			if err := c.Run(); err != nil {
+				errc <- c
+				sterrc <- fmt.Sprintf("%s", stderr)
+			}
+
+			stoutc <- fmt.Sprintf("%s", stdout)
+		}(cmd)
+
+	}
+
+	errCount := 0
+	for stoutc != nil || sterrc != nil {
+		select {
+		case data, ok := <-stoutc:
+			if !ok {
+				stoutc = nil
+				continue
+			}
+			fmt.Fprintf(os.Stdout, data)
+		case data, ok := <-sterrc:
+			if !ok {
+				sterrc = nil
+				continue
+			}
+			fmt.Fprintf(os.Stderr, data)
+		case _, ok := <-errc:
+			if !ok {
+				errc = nil
+				continue
+			}
+			errCount++
+		}
+	}
+	if errCount > 0 {
+		return errors.New("did not finish successfully")
+	}
+	return nil
 }

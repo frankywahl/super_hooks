21bd855b1c19472b2c7211adbb68b7911fef33e3
Franky W <frankywahl@users.noreply.github.com>
2019-11-19 18:35:16 -0800

    Make it match more modern cobra standards

--- cmd/root.go
+++ cmd/root.go
@@ -6,15 +6,18 @@ import (
 	"path"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "super_hooks",
 	Short: "Tool for managing git-hooks",
 	Long: `
-	Tool that changes the git-hooks so that you can share hooks across mulitple git 
-	directories. You include hooks in folder so that each executable can be in charge 
+	Tool that changes the git-hooks so that you can share hooks across mulitple git
+	directories. You include hooks in folder so that each executable can be in charge
 	of only one thing (preserving the SRP rule in your hooks too)`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		file, err := os.Executable()
@@ -43,5 +46,35 @@ func Execute() {
 var Verbose bool
 
 func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.super_hooks.yaml)")
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".super_hooks" (without extension).
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".super_hooks")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
